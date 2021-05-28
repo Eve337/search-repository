8c937540acea615ac1c331f95298ae36f9e87f7a
eve337 <vesunchik1945@gmail.com>
2021-05-28 09:31:29 +0300

    feat: added inf scroll, but doesnt work

--- src/components/ResultsRepos.tsx
+++ src/components/ResultsRepos.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { useState } from 'react';
 import './ResultsRepos.css';
 import SimpleCard from './Card';
 
 const ResultRepos = (props : any) => {
     const { repositories } = props;
     console.log(repositories);
-    const listRepositories = repositories.length !== 0 ? 
-     repositories.items.map((el:any, index: number) => 
+    const listRepositories = repositories.length > 0 && 
+     repositories.map((el:any, index: number) => 
      
      <SimpleCard 
         className = "card"
@@ -16,17 +15,19 @@ const ResultRepos = (props : any) => {
         key = {el.id} 
         url = {el.html_url} 
         creator = {el.owner.login}
-        description = {el.description}/>) : <li>No repositories was found</li>
+        description = {el.description}/>);
+        
+        console.log(listRepositories + ' here');
 
     return (
         <>
-        <ul className="repoList">
+        <ul className="repoList" onScroll={props.handleScroll}>
             {listRepositories}
         </ul>
-        
         </>
     )
 }
+
 
 export default ResultRepos;
 
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,39 +1,56 @@
 import React, { useEffect }  from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ResultRepos from './ResultsRepos';
 
+
 const useStyles = makeStyles((theme) => ({
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+      
     },
   }));
 
 const Searchbar = (props : object) => {
+    const firstRenderRef = useRef(true);
     const classes = useStyles();
     const [search, setSearchInput] = useState('');
-    const [repositories, setRepositories] = useState('');
+    const [repositories, setRepositories] = useState<Array<object>>([]);
     const [pageNumber, setPageNumber] = useState(1);
-    const [loading, setLoading] = useState(true);
-
+    
 
 
     useEffect(()=> {
-
-    }, [pageNumber])
+        if (firstRenderRef.current) {
+            firstRenderRef.current = false;
+          } else {
+            requestGetRepositories(pageNumber);
+          }
+    },[pageNumber]);
+    
 
     const handleChange = (e: any) => {
         setSearchInput(e.target.value);
     }
-    const handleClick = async () => {
-        await fetch(`https://api.github.com/search/repositories?q=${search}&sort=stars`)
+
+    const handleScroll = (e: any) => {
+        console.log('working');
+        const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+        
+        if (scrollHeight - scrollTop === clientHeight) {
+        setPageNumber(prev => prev + 1);
+            console.log('working');
+        }
+    }
+
+    const requestGetRepositories = async (pageNumber: any) => {
+        
+        await fetch(`https://api.github.com/search/repositories?q=${search}&sort=stars&per_page=15&page=${pageNumber}`)
         .then(res => res.json())
         .then(data => {
-            setRepositories(data);
+            console.log(data);
+            setRepositories(reps => [...reps, ...data.items]);
             
             }).then(data => console.log(repositories));
         
@@ -53,11 +70,11 @@ const Searchbar = (props : object) => {
                 <Button 
                     variant="contained" 
                     color="primary" 
-                    onClick={handleClick}>
+                    onClick={() => requestGetRepositories(pageNumber)}>
                     Lets roll
                 </Button>
             </form>
-            <ResultRepos repositories = {repositories} />
+            <ResultRepos repositories = {repositories} handleScroll={handleScroll} />
         </div>
     )
 }
